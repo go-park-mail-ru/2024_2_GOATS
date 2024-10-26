eaae3ffbdacf106000fdb45c0b02ba659a6e6066
UnicoYal <yagmurov.igor@mail.ru>
2024-10-26 15:59:53 +0300

    Schema

--- internal/db/schema/db.sql
+++ internal/db/schema/db.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS movies CASCADE;
+DROP TABLE IF EXISTS actors CASCADE;
+DROP TABLE IF EXISTS movie_actors CASCADE;
 DROP TABLE IF EXISTS collections CASCADE;
 DROP TABLE IF EXISTS movie_collections CASCADE;
 DROP TABLE IF EXISTS genres CASCADE;
@@ -9,8 +11,8 @@ DROP TYPE IF EXISTS movie_type_enum;
 DROP TYPE IF EXISTS sex_enum;
 
 CREATE TABLE public.genres(
-  id serial PRIMARY KEY,
-  title varchar NOT NULL UNIQUE,
+  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title text NOT NULL UNIQUE,
   created_at timestamp WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   updated_at timestamp WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
@@ -19,10 +21,11 @@ CREATE INDEX idx_genres_title ON public.genres(title);
 
 CREATE TYPE public.sex_enum AS ENUM ('male', 'female', 'other', 'secret');
 CREATE TABLE public.users(
-  id serial PRIMARY KEY,
-  username varchar UNIQUE,
-  email varchar NOT NULL UNIQUE,
-  password_hash varchar NOT NULL,
+  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  username text UNIQUE,
+  email text NOT NULL UNIQUE,
+  avatar_url text DEFAULT '/static/user_avatars/default.jpg',
+  password_hash text NOT NULL,
   sex SEX_ENUM,
   birthdate date,
   created_at timestamp WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
@@ -35,10 +38,10 @@ CREATE INDEX idx_users_created_at ON public.users(created_at);
 CREATE INDEX idx_users_updated_at ON public.users(updated_at);
 
 CREATE TABLE public.countries(
-  id serial PRIMARY KEY,
-  title varchar NOT NULL,
-  code varchar NOT NULL,
-  flag_url varchar DEFAULT ''
+  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title text NOT NULL,
+  code text NOT NULL,
+  flag_url text DEFAULT ''
 );
 
 CREATE INDEX idx_countries_title ON public.countries(title);
@@ -46,14 +49,15 @@ CREATE INDEX idx_countries_code ON public.countries(code);
 
 CREATE TYPE public.movie_type_enum AS ENUM ('film', 'serial');
 CREATE TABLE public.movies(
-  id serial PRIMARY KEY,
-  title varchar NOT NULL,
+  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title text NOT NULL,
   description text NOT NULL,
-  card_url varchar DEFAULT '',
-  album_url varchar DEFAULT '',
+  card_url text DEFAULT '',
+  album_url text DEFAULT '',
   release_date date NOT NULL,
   rating decimal(10,2) DEFAULT '0.0',
   movie_type MOVIE_TYPE_ENUM,
+  video_url text DEFAULT '',
   country_id int REFERENCES public.countries(id),
   created_at timestamp WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   updated_at timestamp WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
@@ -64,8 +68,30 @@ CREATE INDEX idx_movies_movie_type ON public.movies(movie_type);
 CREATE INDEX idx_movies_release_date ON public.movies(release_date);
 CREATE INDEX idx_movies_country_id ON public.movies(country_id);
 
+CREATE TABLE public.actors(
+  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  first_name text NOT NULL,
+  second_name text NOT NULL,
+  patronymic text,
+  country_id int REFERENCES public.countries(id),
+  photo_url text DEFAULT '',
+  birthdate date,
+  biography text DEFAULT ''
+);
+
+CREATE INDEX idx_actors_country_id ON public.actors(country_id);
+
+CREATE TABLE public.movie_actors(
+  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  movie_id int REFERENCES public.movies(id),
+  actor_id int REFERENCES public.actors(id)
+);
+
+CREATE INDEX idx_movie_actors_movie_id ON public.movie_actors(movie_id);
+CREATE INDEX idx_movie_actors_actor_id ON public.movie_actors(actor_id);
+
 CREATE TABLE public.movie_genres(
-  id serial PRIMARY KEY,
+  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   movie_id int REFERENCES public.movies(id),
   genre_id int REFERENCES public.genres(id)
 );
@@ -74,10 +100,10 @@ CREATE INDEX idx_movie_genres_movie_id ON public.movie_genres(movie_id);
 CREATE INDEX idx_movie_genres_genre_id ON public.movie_genres(genre_id);
 
 CREATE TABLE public.collections(
-  id serial PRIMARY KEY,
-  title varchar NOT NULL UNIQUE,
-  card_url varchar,
-  album_url varchar,
+  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title text NOT NULL UNIQUE,
+  card_url text,
+  album_url text,
   conditions json,
   created_at timestamp WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   updated_at timestamp WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
@@ -87,7 +113,7 @@ CREATE INDEX idx_collections_title ON public.collections(title);
 CREATE INDEX idx_collections_created_at ON public.collections(created_at);
 
 CREATE TABLE public.movie_collections(
-  id serial PRIMARY KEY,
+  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   movie_id int REFERENCES public.movies(id),
   collection_id int REFERENCES public.collections(id),
   created_at timestamp WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
